a1b0073bce4f1c74964d414a152d5b9b512bc411
Nishtha-Sharma <lit2015039@iiita.ac.in>
2017-02-27 05:16:35 +0530

    Question 2 file

---
+++ Ques2.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+#include "collection.h"
+using namespace std;
+int main()
+{
+    	int i;
+	collection Q2; //Exchanging gifts and calculating happiness and compatibility
+	return 0;
+}
