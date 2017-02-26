9886acba825d4b299b25d840cd0502063f9b473c
Nishtha-Sharma <lit2015039@iiita.ac.in>
2017-02-27 05:09:44 +0530

    Header file for class GIRL

---
+++ GIRL.h
@@ -0,0 +1,15 @@
+#ifndef GIRL_H 			//Header file for class GIRL
+#define GIRL_H
+#include<bits/stdc++.h>
+using namespace std;
+class GIRL{
+	public:
+        	char name[10]; 			//Unique name of girl
+        	int attractiveness; 		//Attractiveness of girl
+        	int maintenance; 		//Maintenance Cost of a girl
+        	int intelligence; 		//Intelligence of girl
+        	int status; 			//Status: committed/single
+        	int criteria; 			//criteria: Most Attractive/Richest/Most Intelligent boy
+        	int type; 			//Type: Choosy/Normal/Desperate
+};
+#endif
