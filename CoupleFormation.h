094a65dc9bdd5ae04bc8f727617a693a232ca1ca
Nishtha-Sharma <lit2015039@iiita.ac.in>
2017-02-27 05:13:04 +0530

    Header file for class CoupleFormation

---
+++ CoupleFormation.h
@@ -0,0 +1,10 @@
+//Header File for class CoupleFormation
+
+#ifndef CoupleFormation_H
+#define CoupleFormation_H
+
+class CoupleFormation{
+	public:
+		CoupleFormation(); 		//Constructor of CoupleFormation
+};
+#endif
