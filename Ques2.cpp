932f8544c4ddaa2ffaae9292933f11a8875549c6
Nishtha-Sharma5 <lit2015039@iiita.ac.in>
2017-04-10 04:21:56 +0530

    Delete Ques2.cpp

--- Ques2.cpp
+++
@@ -1,9 +0,0 @@
-#include<bits/stdc++.h>
-#include "collection.h"
-using namespace std;
-int main()
-{
-    	int i;
-	collection Q2; //Exchanging gifts and calculating happiness and compatibility
-	return 0;
-}
